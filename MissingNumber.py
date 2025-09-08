0f21215b4ffff8d82bd460a23ac1cf5d194f2622
White <69239449+SlightlyOffset@users.noreply.github.com>
2025-09-08 12:25:31 +0700

    Update MissingNumber for more memory efficient

--- MissingNumber.py
+++ MissingNumber.py
@@ -1,15 +1,19 @@
 '''MissingNumber'''
 def main():
     '''main'''
-    original_set = set()
     set_max_range = int(input())
-    compare_set = set(range(1, set_max_range + 1))
+    provided_numbers = set()
+
     while True:
         number_input = int(input())
         if not number_input:
             break
-        original_set.add(number_input)
-    print(*sorted(compare_set - original_set), sep="\n")
+        provided_numbers.add(number_input)
+
+    # Use a generator expression to find missing numbers without creating a large set in memory.
+    # This is much more memory-efficient for a large set_max_range.
+    missing_numbers = (i for i in range(1, set_max_range + 1) if i not in provided_numbers)
+    print(*sorted(missing_numbers), sep="\n")
 
 if __name__ == '__main__':
     main()
