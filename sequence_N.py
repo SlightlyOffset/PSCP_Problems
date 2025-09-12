d8eb895b9ed2d05164b8ad83aa223651741b5f43
White <69239449+SlightlyOffset@users.noreply.github.com>
2025-09-13 06:28:14 +0700

    Upload solutions

---
+++ sequence_N.py
@@ -0,0 +1,15 @@
+'''Sequence N'''
+def main():
+    '''main'''
+    n = int(input())
+    for i in range(1, n+1):
+        output = ''
+        for j in range(1, n+1):
+            if j in (i, n) or j == 1:
+                output += '*'
+            else:
+                output += ' '
+        print(output)
+
+if __name__ == "__main__":
+    main()
