f1de2de5165f9b3d1b85f0428febf682e0932b5c
White <69239449+SlightlyOffset@users.noreply.github.com>
2025-09-22 11:29:47 +0700

    fix misspell

--- Diginity_mod9.py
+++ Diginity_mod9.py
@@ -4,11 +4,11 @@ Diginity (Congruent modulo 9)
 Breakdown: The core principle behind the security system described is the concept of digital root.
 
 Definition: if you divide a number by 9, you will get the same remainder as when you divide
-the zum of its digits by 9. This is a fundamental property in modular arithmetic and is the
+the sum of its digits by 9. This is a fundamental property in modular arithmetic and is the
 basis for the divisibility rule of 9.
 
 It works because of the mathematical property that any number is congruent to
-the zum of its digits modulo 9. When you repeatedly zum the digits, you're essentially
+the sum of its digits modulo 9. When you repeatedly sum the digits, you're essentially
 performing a series of operations that don't change the number's remainder when divided by 9.
 '''
 
