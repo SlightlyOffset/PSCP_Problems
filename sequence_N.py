4bea9109047288af1fb244bac4d710a49f5435d9
White <69239449+SlightlyOffset@users.noreply.github.com>
2025-10-09 23:46:42 +0700

    Slight optimization

--- sequence_N.py
+++ sequence_N.py
@@ -5,11 +5,10 @@ def main():
     for i in range(1, n+1):
         output = ''
         for j in range(1, n+1):
-            if j in (i, n) or j == 1:
+            if j in (i, n, 1):
                 output += '*'
             else:
                 output += ' '
         print(output)
-
 if __name__ == "__main__":
     main()
